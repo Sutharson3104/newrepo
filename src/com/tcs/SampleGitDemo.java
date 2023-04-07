5deb3fd45bfab487113ce0ed8846adea9cf0572c
Sutharson3104 <muppidarisutharson@gmail.com>
2023-04-07 20:57:21 +0530

    new repo

---
+++ src/com/tcs/SampleGitDemo.java
@@ -0,0 +1,11 @@
+package com.tcs;
+
+public class SampleGitDemo {
+	public static void main(String[] args) {
+		
+		System.out.println("gitdemo");
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sutha
+ *
+ */
+module SamplePro {
+}
